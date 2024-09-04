4c1cb84cee6a13ec40456c37e5f8f535f201a187
Mayank-Kapadane <mayank.kapadane2005@gmail.com>
2024-09-04 18:31:14 +0530

    Add key to the file to resolve warning : each child should have unique prop

--- src/MyComponets/Todos.js
+++ src/MyComponets/Todos.js
@@ -1,13 +1,29 @@
 import React from "react";
 import { TodoItem } from "./TodoItem";
 
-
-
 export const Todos = (props) => {
     return (
         <div className="container">
             <h3 className="text-center my-3">Todo's List</h3>
-            <TodoItem todo={props.todo[0]} />
+            {props.todo.map((todo) => {
+                // return <TodoItem todo={todo} />   // return only one line
+
+                // console.log(todo);
+                // to return multiple line you can use <>...</>
+
+                // return <>
+                //     {/* <h3>this</h3> */}
+                //     <TodoItem todo={todo} onDelete={props.onDelete} />
+
+                // </>;
+               
+
+                //  To avoid warning : each child should have unique prop 
+                return <div key={todo.sno}> 
+                        <TodoItem todo={todo} onDelete={props.onDelete} />
+                    </div>
+
+            })}
         </div>
     );
-}
+};
